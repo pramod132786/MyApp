aa50028ea64e9c1ccd12284a923a8385419cddfe
pramod132786 <155069426+pramod132786@users.noreply.github.com>
2024-03-13 19:00:32 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,29 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
-                        <a className="navbar-brand fs-5 fw-bold" href="#" >Pramod-App</a>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <a className="navbar-brand fs-5 fw-bold" href="#">Pramod-App</a>
                         <div className="d-flex justify-content-end">
-
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="/register">Register</a>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/register">Register</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="/login">Login</a>
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="/login">Login</a>
                                 </li>
                             </ul>
                         </div>
-                        {/* <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        {/* <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form> */}
                     </div>
                 </div>
@@ -34,4 +33,5 @@ const Home = () => {
         </>
     )
 }
+
 export default Home;
