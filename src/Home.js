8e7ddd13bde70889bd26f908fb351bf4c50bc05f
pramod132786 <155069426+pramod132786@users.noreply.github.com>
2024-03-14 10:07:16 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import { FaSmile } from "react-icons/fa";
 
 function Home() {
     return (
-<<<<<<< HEAD
+
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" data-bs-theme="dark" >
             <Container>
                 <Navbar.Brand href="/" >Pramod-App <FaSmile/></Navbar.Brand>
@@ -23,38 +23,7 @@ function Home() {
             </Container>
         </Navbar>
     );
-=======
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand fs-5 fw-bold" href="#">Pramod-App</a>
-                        <div className="d-flex justify-content-end">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/register">Register</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link active" href="/login">Login</a>
-                                </li>
-                            </ul>
-                        </div>
-                        {/* <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
->>>>>>> aa50028ea64e9c1ccd12284a923a8385419cddfe
+
 }
 
 export default Home;
